19670ca1cab494c15badf8e2067156413074a28d
techis <techis@users.noreply.github.com>
2023-10-07 16:35:51 -0500

    Added some console logging and moved files after processing

--- uploadtQSL.ps1
+++ uploadtQSL.ps1
@@ -5,10 +5,17 @@ $tqslInstall = "C:\Program Files (x86)\TrustedQSL"
 $UploadDir = (get-location).Path
 $DefaultLocation = "QTH"
 $BatchFile = "C:\tQSLLogs\uploadtQSL.bat"
+$processedDir = $LogDir+"\Processed\"
 
 if (!(Test-Path -Path $LogDir -PathType Container)) {
+    Write-host "Creating $LogRootDir\$logDate"
     New-Item -Path $LogRootDir -Name $logDate -ItemType Directory
 }
+if (!(Test-Path -Path $processedDir -PathType Container)) {
+    Write-host "Creating $logDir\Processed"
+    New-Item -Path $logdir -Name "Processed" -ItemType Directory
+}
+
 
 $LogsToUpload = Get-ChildItem -Path $UploadDir | Where-Object {$_.Name -Like "*.adi"} | Select-Object -Property Name
 
@@ -16,7 +23,7 @@ $LogsToUpload = Get-ChildItem -Path $UploadDir | Where-Object {$_.Name -Like "*.
 foreach ($log in $LogsToUpload){
     $location = $null
     $path = $null
-    write-host $log.name
+    write-host "Found $($log.name)"
     if ($Log.name -like "*@*") {
         $location = ($log.Name -split('@'))[1].substring(0,6)
            
@@ -25,8 +32,13 @@ foreach ($log in $LogsToUpload){
         $location = $DefaultLocation
     }
     $Path = $UploadDir+"\"+$log.Name
-    Write-host $path $Location
+
+    Write-host "Uploading $($log.name)"
+
     &"$BatchFile" $tqslInstall $Location $Path $logdir
+
+    Write-host "Moving $($log.name) to $ProcessedDir"
+    Move-Item -Path $path -Destination "$ProcessedDir\$($log.name)"
 }
 
 
